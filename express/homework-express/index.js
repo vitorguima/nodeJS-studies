83e9eee3a6ecfb918e5b11f8b53be0a213561877
Vitor <vitorguima@gmail.com>
2021-09-30 16:58:30 -0300

    feito ate o ex 4 da tarefa

--- express/homework-express/index.js
+++ express/homework-express/index.js
@@ -2,10 +2,31 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
+app.use(express.json());
 app.use(cors());
 
 app.get('/ping', (req, res) => {
   res.status(200).json({ message: 'Hello, <nome do usuário>!' });
+})
+
+app.post('/hello', (req, res) => {
+  const { name } = req.body;
+  res.status(201).json({ message: `Hello, ${name}` });
+})
+
+app.post('/greetings', (req, res) => {
+  const { name, age } = req.body;
+
+  if (age > 17) {
+    return res.status(201).json({ message: `Hello, ${name}`, age });
+  }
+
+  return res.status(401).json({ message: "Unauthorized" });
+})
+
+app.put('/users/:name/:age', (req, res) => {
+  const { name, age } = req.params;
+  return res.status(201).json({ message: `Seu nome é ${name} e você tem ${age} anos de idade` });
 })
 
 app.listen(3000, () => console.log('Rodando na porta 3000'));
