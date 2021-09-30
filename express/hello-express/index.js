0f2700e1dbc0fc2baa45e47e920a210df55f66ea
Vitor <vitorguima@gmail.com>
2021-09-30 14:51:56 -0300

    exercicio de aula finalizado

--- express/hello-express/index.js
+++ express/hello-express/index.js
@@ -46,6 +46,29 @@ app.post('/recipes', function (req, res) {
   res.status(201).json({ message: 'Recipe created successfully!'});
 });
 
+app.put('/recipes/:id', function (req, res) {
+  const { id } = req.params;
+  const { name, price } = req.body;
+  const recipeIndex = recipes.findIndex((r) => r.id === parseInt(id));
+
+  if (recipeIndex === -1) return res.status(404).json({ message: 'Recipe not found!' });
+
+  recipes[recipeIndex] = { ...recipes[recipeIndex], name, price };
+
+  res.status(204).end();
+});
+
+app.delete('/recipes/:id', function (req, res) {
+  const { id } = req.params;
+  const recipeIndex = recipes.findIndex((r) => r.id === parseInt(id));
+
+  if (recipeIndex === -1) return res.status(404).json({ message: 'Recipe not found!' });
+
+  recipes.splice(recipeIndex, 1);
+
+  res.status(204).end();
+});
+
 app.get('/drinks/search', function (req, res) {
   const { name } = req.query;
   const filteredDrinks = drinks.filter((d) => d.name.includes(name));
@@ -69,6 +92,34 @@ app.post('/drinks', function (req, res) {
   const { id, name, price,  waitTime } = req.body;
   drinks.push({ id, name, price,  waitTime });
   res.status(201).json({ message: 'Drink created successfully!'});
+});
+
+app.put('/drinks/:id', function (req, res) {
+  const { id } = req.params;
+  const { name, price } = req.body;
+
+  const drinkIndex = drinks.findIndex((d) => d.id === parseInt(id));
+
+  if (drinkIndex === -1) return res.status(404).json({ message: 'Drink not found!' });
+
+  drinks[recipeIndex] = { ...drinks[drinkIndex], name, price };
+
+  res.status(204).end();
+});
+
+app.delete('/drinks/:id', function (req, res) {
+  const { id } = req.params;
+  const drinkIndex = drinks.findIndex((r) => r.id === parseInt(id));
+
+  if (drinkIndex === -1) return res.status(404).json({ message: 'drink not found!' });
+
+  drinks.splice(drinkIndex, 1);
+
+  res.status(204).end();
+})
+
+app.all('*', function (req, res) {
+  return res.status(404).json({ message: `Rota '${req.path}' não existe!`});
 });
 
 app.listen(3000, () => {
