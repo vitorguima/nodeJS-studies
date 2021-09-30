74770ff144b7c047e22beaa2ab97ac63bebfaa52
Vitor <vitorguima@gmail.com>
2021-09-30 12:22:54 -0300

    finaliza codigos para POST method

--- express/hello-express/index.js
+++ express/hello-express/index.js
@@ -1,5 +1,8 @@
 const express = require('express');
+// const bodyParser = require('body-parser');
+
 const app = express();
+app.use(express.json());
 
 const recipes = [
   { id: 1, name: 'Lasanha', price: 40.0, waitTime: 30 },
@@ -18,13 +21,55 @@ const drinks = [
   { id: 6, name: 'Água Mineral 500 ml', price: 5.0 },
 ];
 
+app.get('/recipes/search', function (req, res) {
+  const { name, maxPrice, minPrice } = req.query;
+  const filteredRecipes = recipes.filter((r) => r.name.includes(name) && r.price < parseInt(maxPrice) && r.price > parseInt(minPrice));
+  res.status(200).json(filteredRecipes);
+});
+
+app.get('/recipes/:id', function (req, res) {
+  const { id } = req.params;
+  const recipe = recipes.find((r) => r.id === parseInt(id));
+
+  if (!recipe) return res.status(404).json({ message: 'Recipe not found!'});
+
+  res.status(200).json(recipe);
+});
+
 app.get('/recipes', function (req, res) {
   res.json(sortList(recipes));
 });
 
+app.post('/recipes', function (req, res) {
+  const { id, name, price,  waitTime } = req.body;
+  recipes.push({ id, name, price,  waitTime });
+  res.status(201).json({ message: 'Recipe created successfully!'});
+});
+
+app.get('/drinks/search', function (req, res) {
+  const { name } = req.query;
+  const filteredDrinks = drinks.filter((d) => d.name.includes(name));
+  res.status(200).json(filteredDrinks);
+})
+
+app.get('/drinks/:id', function (req, res) {
+  const { id } = req.params;
+  const drink = drinks.find((d) => d.id === parseInt(id));
+
+  if (!drink) return res.status(404).json({ message: 'Drink not found!' });
+
+  res.status(200).json(drink);
+})
+
 app.get('/drinks', function (req, res) {
   res.json(sortList(drinks));
 })
+
+app.post('/drinks', function (req, res) {
+  const { id, name, price,  waitTime } = req.body;
+  drinks.push({ id, name, price,  waitTime });
+  res.status(201).json({ message: 'Drink created successfully!'});
+});
 
 app.listen(3000, () => {
   console.log('Aplicação ouvindo na porta 3001');
